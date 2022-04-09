8c34bcc406fd2c91918ef3d460861c619c059711
Aprian Firlanda Imani <aprianfirlanda@gmail.com>
2022-04-09 11:38:17 +0700

    create simple REST API hello world

---
+++ src/main/kotlin/com/aprianfirlanda/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package com.aprianfirlanda
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWolrd(): String ="Hello, this is a REST endpoint"
+}
